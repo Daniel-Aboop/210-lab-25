a59034a825136fb68639ef27f5099808552e123d
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-10-26 11:39:06 -0700

    11:38 AM had to go do something

--- main.cpp
+++ main.cpp
@@ -23,6 +23,22 @@ int main() {
     vector<string> racer1;
     set<string> racer2;
     list<string> racer3;
+    string temp;
+    
+    while(file>>temp){
+        racer1.push_back(temp);
+    }
+    temp="";
+    while(file>>temp){
+        racer2.insert(temp);
+    }
+    temp="";
+    while(file>>temp){
+        racer3.push_back(temp);
+    }
+
+
+    file.close();
     return 0;
 }
 
